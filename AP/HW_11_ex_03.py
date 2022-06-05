424d0b92d545c5e61e655c5cbc156448b502e7a2
Andrii V <kidstonek@gmail.com>
2022-06-05 18:39:31 +0300

    HW AP11 - ex 3-10 done

---
+++ AP/HW_11_ex_03.py
@@ -0,0 +1,44 @@
+"""
+У класса Point к механизму setter свойств x и y добавьте проверку на вводимое значение.
+Разрешите устанавливать значения свойств x и y для экземпляра класса, только, если они
+имеют числовое значение (int или float).
+Пример:
+
+point = Point("a", 10)
+
+print(point.x)  # None
+print(point.y)  # 10
+"""
+class Point:
+    def __init__(self, x, y):
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if (type(x) == int) or (type(x) == float):
+            self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if (type(y) == int) or (type(y) == float):
+            self.__y = y
+
+
+def main():
+    pass
+
+
+if '__main__' == __name__:
+    main()
+
